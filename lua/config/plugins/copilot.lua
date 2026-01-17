a1434d13196ec00bf402e55d8b98605eea669769
Kiril Strezikozin <kirilstrezikozin@gmail.com>
2026-01-17 14:51:10 +0100

    plugins/copilot: initial commit
    Signed-off-by: Kiril Strezikozin <kirilstrezikozin@gmail.com>

---
+++ lua/config/plugins/copilot.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    'github/copilot.vim',
+    config = function()
+      vim.g.copilot_no_tab_map = true
+      vim.keymap.set("i", "<C-;>", 'copilot#Accept("<CR>")', { silent = true, expr = true, replace_keycodes = false })
+    end,
+  }
+}
